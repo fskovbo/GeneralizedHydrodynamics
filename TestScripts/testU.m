317e71c400774618d14d24e9db32a96568a80fb6
fskovbo <f.skovbo@hotmail.com>
2019-11-03 22:47:00 +0100

    Added path tracing

--- TestScripts/testU.m
+++ TestScripts/testU.m
@@ -38,11 +38,12 @@ LLS         = LiebLinigerSolver(x_array, k_array, couplings);
 theta_init  = LLS.calcThermalState(T, couplings_i);
 
 
-[theta_t, u_t] = LLS.propagateTheta(theta_init, t_array);
+[theta_t, u_t, w_t] = LLS.propagateTheta(theta_init, t_array);
 
 %% Test u - should follow theta(x,t,lambda) = theta( u(x,t,lambda), 0, lambda)
 Nsamples    = 4;
 t_idx       = floor(linspace(1,length(t_array),Nsamples));
+
 
 for i = 1:Nsamples
     for j = 1:Ntypes
@@ -52,12 +53,15 @@ for i = 1:Nsamples
         uj_t    = u_t{t_idx(i)};
         uj_t    = squeeze( uj_t(:,:,j,:,:) );
         
+        wj_t    = w_t{t_idx(i)};
+        wj_t    = squeeze( wj_t(:,:,j,:,:) );
+        
         u_int   = permute( uj_t, [2 1]); % spatial index first
+        w_int   = permute( wj_t, [2 1]); % spatial index first
         the_int = permute(theta_init(:,:,j,:,:), [5 1 3 2 4]); % spatial index first
-        r_int   = repmat(k_array, M, 1);
 
         % interpolates theta(x) to u(t,x,lambda)
-        theta_u = interp2( k_array, x_array, the_int, r_int(:), u_int(:), 'spline');
+        theta_u = interp2( k_array, x_array, the_int, w_int(:), u_int(:), 'spline');
         theta_u = reshape(theta_u, M, N);
         theta_u = permute(theta_u, [2 1]);
 
@@ -90,10 +94,14 @@ end
 
 %% trajectory plot
 Umat = zeros(N,1,Ntypes,1,M,length(t_array));
+Wmat = zeros(N,1,Ntypes,1,M,length(t_array));
 
 for i = 1:length(t_array)
     Umat(:,:,:,:,:,i) = u_t{i};
+    Wmat(:,:,:,:,:,i) = w_t{i};
 end
+
+
 
 
 rapids = 10:10:60;
@@ -121,17 +129,56 @@ end
 
 %%
 
+endpoints = [ 40 64; 70, 70; 100, 100];
+
 for j = 1:Ntypes
-    x = squeeze(Umat(50,1,1,1,50,:));
-    lambda = repmat(50, size(x));
+    
+    the_t   = theta_t{end};
+    the_t   = squeeze( the_t(:,:,j,:,:) );
+
+    
+    col     = t_array;
     
     figure
+
+    ax1 = axes;
+    imagesc(x_array, k_array, the_t )
+    set(gca,'YDir','normal') 
+    
+    xlabel('Space')
+    ylabel('Rapidity')
+
+    xlim([-xmax , xmax])
+    ylim([-kmax , kmax])
+    
+    
+    ax2 = axes;
     hold on
-%     scatter( x, lambda )
-    scatter( Umat(40,1,1,1,50,:) , repmat(k_array(40), length(t_array),1))
-    scatter( Umat(50,1,1,1,50,:) , repmat(k_array(50), length(t_array),1))
-    scatter( Umat(60,1,1,1,50,:) , repmat(k_array(60), length(t_array),1))
-    scatter( Umat(70,1,1,1,50,:) , repmat(k_array(70), length(t_array),1))
+    
+    for i = 1:size(endpoints,1)
+        x       = flip(squeeze(Umat(endpoints(i,1),1,1,1,endpoints(i,2),:)));
+        lambda  = flip(squeeze(Wmat(endpoints(i,1),1,1,1,endpoints(i,2),:)));
+
+        scatter(x(:),lambda(:),[],col,'filled')
+    end
     
     
+    %Link them together
+    linkaxes([ax1,ax2])
+    %Hide the top axes
+    ax2.Visible = 'off';
+    ax2.XTick = [];
+    ax2.YTick = [];
+    %Give each one its own colormap
+    colormap(ax1,'hot')
+    colormap(ax2,'cool')
+    
+    
+    c = colorbar;
+    c.Label.String = 'Time';
+    c.Label.FontSize = 12;
+    
+    pos = ax2.Position;
+    set([ax1,ax2],'Position',pos);
 end
+
